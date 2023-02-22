b8e4c7e651cf61b6712e9378aab7e53af1a6e7a4
Julien Kipp <julien@doji-tech.com>
2023-02-22 02:02:34 +0100

    Added sphere generation code.

---
+++ Assets/SphereGeneration/Editor/OctahedronSphereWizard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class OctahedronSphereWizard : ScriptableWizard {
+	
+	[MenuItem("Assets/Create/Octahedron Sphere")]
+	private static void CreateWizard () {
+		ScriptableWizard.DisplayWizard<OctahedronSphereWizard>("Create Octahedron Sphere");
+	}
+	
+	public int level = 6;
+	public float radius = 1f;
+	
+	private void OnWizardCreate () {
+		string path = EditorUtility.SaveFilePanelInProject("Save Octahedron Sphere", "Octahedron Sphere", "asset", "Specify where to save the mesh.");
+		if (path.Length > 0) {
+			Mesh mesh = OctahedronSphereCreator.Create(level, radius);
+			MeshUtility.Optimize(mesh);
+			AssetDatabase.CreateAsset(mesh, path);
+			Selection.activeObject = mesh;
+		}
+	}
+}
